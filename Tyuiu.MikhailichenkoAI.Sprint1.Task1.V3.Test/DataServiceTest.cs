c6d7ad2edcc7a839a4be7f33f2d2fe5f15c41eda
MikhailichenkoArtemy <Tyuiu.MikhailichenkoAI@outlook.com>
2026-09-14 22:58:54 +0500

    Task1

---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task1.V3.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task1.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = -3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task1.V3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task1.V3.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task1.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Михайличенко А. И. | ИИПБ26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Михайличенко А. И. | ИИПБ26-1                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x-y)/(x+3)+3 и печатает его на экране.  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+
+            double x,y;
+
+            Console.WriteLine("Введите значение переменной X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x,y));
+
+            Console.ReadLine();
+        }
+    }
+}

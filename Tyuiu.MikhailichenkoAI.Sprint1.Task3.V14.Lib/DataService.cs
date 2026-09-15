49329662faf0175fc4b3d35894a95869bd855cac
MikhailichenkoArtemy <Tyuiu.MikhailichenkoAI@outlook.com>
2026-09-15 22:29:14 +0500

    Task3

---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task3.V14.Lib
+{
+    public class DataService : ISprint1Task3V14
+    {
+        public double ReverseNumber(double number)
+        {
+            
+            int num = Convert.ToInt32(number);
+
+            
+            int a = num / 100;          
+            int b = (num / 10) % 10;    
+            int c = num % 10;          
+
+            
+            double result = (c * 100) + (b * 10) + a;
+
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task3.V14.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task3.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            
+            double number = 456;
+
+            
+            var res = ds.ReverseNumber(number);
+
+            
+            Assert.AreEqual(654.0, res);
+        }
+    }
+}

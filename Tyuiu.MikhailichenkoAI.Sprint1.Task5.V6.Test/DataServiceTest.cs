90594d5ea2e8fd215457a784854883e216299f66
MikhailichenkoArtemy <Tyuiu.MikhailichenkoAI@outlook.com>
2026-09-15 23:13:48 +0500

    Task5

---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            
+            int k = 8;
+
+            int wait = 1;
+
+            
+            var res = ds.Calculate(k);
+
+            
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task5.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1 | Выполнил: Михайличенко А. И.                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти, на какой день недели (1-7) приходится k-й день года,             *");
+            Console.WriteLine("* если 1 января - это понедельник.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер дня в году (k от 1 до 365):");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            Console.WriteLine(ds.Calculate(k));
+
+            Console.ReadLine();
+        }
+    }
+}

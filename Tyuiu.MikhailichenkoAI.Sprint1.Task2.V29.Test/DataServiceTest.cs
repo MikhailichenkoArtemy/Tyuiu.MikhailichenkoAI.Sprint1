1ad17205040d6b18c73d29bdcf46b485d3743890
MikhailichenkoArtemy <Tyuiu.MikhailichenkoAI@outlook.com>
2026-09-15 21:36:32 +0500

    Task2

---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task2.V29.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int seconds = 130;
+            var res = ds.ConvertSecondsToHours(130);
+            Assert.AreEqual(2, res); 
+        }
+    }
+}
---
+++ Tyuiu.MikhailichenkoAI.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.MikhailichenkoAI.Sprint1.Task2.V29.Lib;
+namespace Tyuiu.MikhailichenkoAI.Sprint1.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Михайличенко А. И. | ИИПБ26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Арифметические операторы в C#                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Михайличенко А. И. | ИИПБ26-1                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+
+            int seconds;
+
+            Console.WriteLine("Введите время в секундах (целое число):");
+            seconds = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.ConvertSecondsToHours(seconds));
+            Console.ReadLine();
+        }
+    }
+}
